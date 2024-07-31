e9eb2c4401a86b68ded0781d4da42ea0a7d4ba34
Raphael Ndolo <raphaelndolo@Raphaels-MacBook-Pro.local>
2024-07-31 15:03:03 +0300

    Update View to accept password and check its validation on length

--- password_validators/views.py
+++ password_validators/views.py
@@ -1,6 +1,15 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from password_validators.validate_password import validate_password
 
+@api_view(["POST"])
 class ValidatePassword(APIView):
     def put(self, request, *args, **kwargs):
         password = self.request.data["password"]
+        validate_password = validate_password(password)
+
+        if isinstance(validate_password, str):
+            return Response({"message":validate_password})
+        return Response({"password": validate_password})
